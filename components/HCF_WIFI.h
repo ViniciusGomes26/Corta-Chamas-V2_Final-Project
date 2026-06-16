6336b90d94bca48448c904f2469a3661e07908fd
ViniciusGomes <viniciusbatista900@gmail.com>
2026-06-16 16:09:54 -0300

    Delete components directory

--- components/HCF_WIFI.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HCF_WIFI_H
-    #define __HCF_WIFI_H
-
-    #include "esp_err.h"
-
-    void wifi_init(void);
-    esp_err_t wifi_connect_sta(const char * ssid, const char * pwd, int timeout);
-    void wifi_disconnect(void);
-#endif
